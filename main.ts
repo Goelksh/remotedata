fa943855948446eaa153d1c3b0d1238b00edb45f
Kshitij Goel <68154931+Goelksh@users.noreply.github.com>
2020-08-07 14:34:45 +0530

    removed animation as it was missing data

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
 radio.onReceivedString(function (receivedString) {
     serial.writeLine("" + radio.receivedPacket(RadioPacketProperty.Time) + "," + radio.receivedPacket(RadioPacketProperty.SignalStrength) + "," + receivedString)
-    basic.showIcon(IconNames.SmallSquare)
-    basic.showIcon(IconNames.Square)
 })
 radio.setGroup(20)
 basic.showString("RL")
